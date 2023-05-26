b7121e9c68024475162d4bf719990c50142b3a6e
AlekseyAgaltsov <AlekseyAgaltsov@gmail.com>
2023-05-26 08:41:24 +0300

    283

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -145,3 +145,16 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '55687606592-07vk7ng2dref4rjpub9igenficcvtv4i.ap
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Google Client Secret
 
 # python manage.py runserver_plus --cert-file cert.crt
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
