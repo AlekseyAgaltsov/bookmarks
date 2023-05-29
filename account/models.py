a707b39aa6281b52361dd79b6b480d1091f31504
AlekseyAgaltsov <AlekseyAgaltsov@gmail.com>
2023-05-29 08:47:18 +0300

    288

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 
 
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
@@ -13,22 +14,4 @@ class Profile(models.Model):
         return f'Profile of {self.user.username}'
 
 
-class Image(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name='images_created',
-                             on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, blank=True)
-    url = models.URLField(max_length=2000)
-    image = models.ImageField(upload_to='images/%Y/%m/%d')
-    description = models.TextField(blank=True)
-    created = models.DateField(auto_now_add=True)
 
-    class Meta:
-        indexes = [
-            models.Index(fields=['-created']),
-        ]
-        ordering = ['-created']
-
-    def __str__(self):
-        return self.title
